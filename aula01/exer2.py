e3f2dfb9ac53c9d105ef0273442a77c746f5f3e5
BellkaDEV <kallebferbot1234@gmail.com>
2026-02-26 08:25:59 -0300

    correção

--- aula01/exer2.py
+++ aula01/exer2.py
@@ -1,12 +1,10 @@
-# Desenvolver um programa em Python que calcule o salário semanal de um trabalhador. As entradas são: o número de horas trabalhadas
-# na semana e o valor da hora. Até 40 h/semana não se acrescenta nenhum adicional. Acima de 40h e até 60h há um bônus de 50% para
-# essas horas. Acima de 60h há um bônus de 100% para essas horas.
-
 hrs = float(input("Horas trabalhadas: "))
 valor = float(input("Salário por hora: "))
 sal = hrs * valor
 if hrs > 40 and hrs < 60:
-    sal = sal * 1.5
-elif hrs >= 60:
-    sal = sal * 26
+    sal = (hrs * valor) + (hrs - 40)* valor * 1.5
+elif hrs > 60:
+    sal = (hrs * valor) + (hrs - 60)* valor * 2
+else:
+    sal = hrs * valor
 print(sal)
